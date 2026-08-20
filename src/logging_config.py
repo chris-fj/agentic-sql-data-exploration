5802dfc47738949182e87bfd3c92e67169d8dfb8
Christian Fortich Jassan <christianfortich@hotmail.com>
2026-08-20 18:42:08 +0200

    Fix logging configuration
    Modified the logging configuration so it's used by both the frontend and
    the backend

--- src/logging_config.py
+++ src/logging_config.py
@@ -28,7 +28,7 @@ LOGGING_CONFIG: dict = {
     "disable_existing_loggers": False,
     "formatters": {
         "default": {
-            "format": "%(asctime)s %(levelname)-8s %(name)s %(message)s",
+            "format": "%(asctime)s - %(levelname)-8s - %(funcName)s - %(message)s",
             "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     },
